77ce57a58555b4a831a81e86728dac63cd61869d
Jan Bölsche <regular.gonzales@googlemail.com>
2017-08-17 17:55:05 +0200

    Decorate hyperscript

--- test.js
+++ test.js
@@ -1,17 +1,26 @@
-const hs = require('hyperscript')
+const hs = decorate(require('hyperscript'))
 const ho = require('hyperobj')
 const observable = require('observable')
 
-function cleanup(h) {
-  (h.nestedContexts || []).forEach(cleanup)
-  h.cleanup()
+function cleanup() {
+  (this._nestedContexts || []).forEach((ctx)=>cleanup.call(ctx))
+  this._cleanup()
 }
 
-function context(h) {
-  let nh = hs.context()
-  h.nestedContexts = (h.nestedContexts || [])
-  h.nestedContexts.unshift(nh)
+function context() {
+  let nh = hs._context()
+  nh = decorate(nh)
+  this._nestedContexts = (this._nestedContexts || [])
+  this._nestedContexts.unshift(nh)
   return nh
+}
+
+function decorate(h) {
+  h._cleanup = h.cleanup
+  h.cleanup = cleanup.bind(h)
+  if (h.context) h._context = h.context
+  h.context = context.bind(h)
+  return h
 }
 
 const render = ho(
@@ -22,17 +31,17 @@ const render = ho(
       Object.keys(v).map( (k)=> {
         const msg = observable()
         return (function(h) {
-          msg('ctx')
+          msg('clickme')
           return h('li', [
             h('em.key', h('span', msg), this.call(this, k), {
               onclick: function() {
-                msg('clicked')
-                cleanup(h)
+                msg('I was clicked')
+                h.cleanup()
               }
             }),
             h('span.value', this.call(this, v[k], k))
           ])
-        }).call(this, this.ctx = context(h))
+        }).call(this, this.ctx = h.context())
       })
     )
   },
